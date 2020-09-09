a59a35fff29f1bd270a474c8f2a5575e5f4cb439
Robert Driskell <driskell.ba@gmail.com>
2020-09-09 18:54:11 -0500

    destructuring practice added to objects.js

--- js_basics/objects.js
+++ js_basics/objects.js
@@ -1,11 +1,22 @@
 const person = {
     name: "Bobby",
     age: 29,
-    greet()  {
-        console.log("Hi, I'm " + this.name + " and I'm " + this.age)
-    }
-}
+    greet() {
+        console.log("Hi, I'm " + this.name + " and I'm " + this.age);
+    },
+};
 
 console.log(person);
 
 person.greet();
+
+// destructuring
+const printName = ({ name }) => {
+    console.log(name);
+};
+
+printName(person);
+
+const { name, age } = person;
+
+console.log(name, age)
