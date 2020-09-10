22897b08904ea2f5d1037966376b6b6a288d561c
Robert Driskell <driskell.ba@gmail.com>
2020-09-10 12:01:32 -0500

    section 3 - sending html in a complicated way

--- s3_basics/app.js
+++ s3_basics/app.js
@@ -1,7 +1,17 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    // hard exits, don't use this
+    // process.exit();
+
+    console.log(req.url, req.method, req.headers);
+
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Testing sending HTML</title></head>");
+    res.write("<body><h1>Hey, I am sending this from Node</h1></body>");
+    res.write("</html>");
+    res.end();
 });
 
 server.listen(3000);
