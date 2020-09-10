66980d569547249087a03acd758d9d09e46b541c
Robert Driskell <driskell.ba@gmail.com>
2020-09-10 12:08:27 -0500

    section 3 - conditionally rendering html

--- s3_basics/app.js
+++ s3_basics/app.js
@@ -4,7 +4,17 @@ const server = http.createServer((req, res) => {
     // hard exits, don't use this
     // process.exit();
 
-    console.log(req.url, req.method, req.headers);
+    const url = req.url;
+
+    if (url === "/") {
+        res.write("<html>");
+        res.write("<head><title>Testing sending HTML</title></head>");
+        res.write(
+            "<body><form action='/message' method='POST'><input type='text'><button type='submit'>Send</button></input></form></body>"
+        );
+        res.write("</html>");
+        return res.end();
+    }
 
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
