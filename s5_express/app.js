d6a5aea4d97206e88e346bec9eb9b6675193c487
Robert Driskell <driskell.ba@gmail.com>
2020-09-10 17:14:26 -0500

    section 5 - first assignment done

--- s5_express/app.js
+++ s5_express/app.js
@@ -2,14 +2,14 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("In the middleware!");
-    next(); // continues to the next middleware
+app.use('/add-product', (req, res, next) => {
+    console.log("Add Product");
+    res.send("<h1>Add Product</h1>");
 });
 
-app.use((req, res, next) => {
-    console.log("In another middleware!");
-    res.send('<h1>Hello from Express!</h1>');
+app.use('/', (req, res, next) => {
+    console.log("In /");
+    res.send("<h1>Hello from Express!</h1>");
 });
 
 app.listen(3000);
