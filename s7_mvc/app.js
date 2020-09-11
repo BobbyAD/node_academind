8f349b68fbac7c4686453d785268987befd601e5
Robert Driskell <driskell.ba@gmail.com>
2020-09-11 17:16:21 -0500

    moved files to section 7

---
+++ s7_mvc/app.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const expressHbs = require("express-handlebars");
+
+const app = express();
+
+app.set("view engine", "ejs");
+// views is default, would change if I was using another folder name
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoutes.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render("404", { pageTitle: "404 Page Not Found", path: false });
+});
+
+app.listen(3000);
