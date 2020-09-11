45c0924de260c6da2129fd4bb382c35b55fb9145
Robert Driskell <driskell.ba@gmail.com>
2020-09-11 17:13:15 -0500

    s6 404 bug fixed

--- s6_dynamic_content/app.js
+++ s6_dynamic_content/app.js
@@ -21,7 +21,7 @@ app.use(shopRoutes);
 
 app.use((req, res, next) => {
     // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-    res.status(404).render("404", { pageTitle: "404 Page Not Found" });
+    res.status(404).render("404", { pageTitle: "404 Page Not Found", path: false });
 });
 
 app.listen(3000);
