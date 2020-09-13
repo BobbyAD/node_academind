6ec3b07f5139b3bcdd8d64ac9eb001dab8d0bade
Robert Driskell <driskell.ba@gmail.com>
2020-09-13 15:03:54 -0500

    section 9 - product details finished

--- s9_dynamic_routes/controllers/shop.js
+++ s9_dynamic_routes/controllers/shop.js
@@ -12,11 +12,14 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res) => {
     const prodId = req.params.productId;
-    Product.findById(prodId, product => {
-        console.log(product);
-    })
-    res.redirect("/");
-}
+    Product.findById(prodId, (product) => {
+        res.render("shop/product-detail", {
+            product: product,
+            pageTitle: product.title,
+            path: "/products",
+        });
+    });
+};
 
 exports.getIndex = (req, res, next) => {
     Product.fetchAll((products) => {
