8b291694519d7b1408e84d50da8be8c9a0c74ffd
Robert Driskell <driskell.ba@gmail.com>
2020-09-14 17:47:58 -0500

    section 10 - console logging dummy data

--- s10_databases/util/database.js
+++ s10_databases/util/database.js
@@ -3,6 +3,8 @@ const mysql = require("mysql2");
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    database: 'node-complete',
-    password: process.env.DB_PASSWORD
+    database: "node-complete",
+    password: process.env.DB_PASS,
 });
+
+module.exports = pool.promise();
